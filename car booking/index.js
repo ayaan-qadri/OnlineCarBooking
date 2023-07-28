7db2f14b46a206382fa1286baf4932092ea8ebbf
PentW0lf <31568025+PentW0lf@users.noreply.github.com>
2023-07-28 17:27:42 +0530

    Delete car booking directory

--- car booking/index.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector(".menu");
-menu.addEventListener("click",()=>{
-    
-    const menus = document.querySelector(".menu-btns");
-    menus.classList.toggle("toggleMenu");
-    document.body.classList.toggle("active")
-})
